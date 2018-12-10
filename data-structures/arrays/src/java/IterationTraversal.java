9fe1f54097bfe440c605368696aec4acd3267bb6
awkonecki.developer <awkonecki.developer@gmail.com>
2018-12-09 17:19:19 -0800

    [FTR] Exercise 2 - Print each element
    Feature Description
    
    Print each integer within the array.
    
    Related Issues
    - N/A

--- data-structures/arrays/src/java/IterationTraversal.java
+++ data-structures/arrays/src/java/IterationTraversal.java
@@ -1,7 +1,7 @@
 public class IterationTraversal {
 
     // #1 find max, min, average.
-    public int findMax(int [] values) {
+    public static int findMax(int [] values) {
         // traversal each one.
 
         // error checking input / input validation
@@ -20,12 +20,57 @@ public class IterationTraversal {
         }
 
         for (int index = 0; index < values.length; index++) {
-
+            if (values[index] > max) {
+                max = values[index];
+            }
         }
 
+        return max;
+    }
+
+    public static int findMin(int [] values) {
+        // error checking / input validation
+
+        int min = values[0];
+
+        for (int number : values) {
+            if (number < min) {
+                min = number;
+            }
+        }
+
+        return min;
+    }
+
+    public static int computeSum(int [] values) {
+        // error checking / input validation
+
+        int sum = 0;
+
+        for (int number : values) {
+            // overflow / underflow being ignored.
+            sum += number;
+        }
+
+        return sum;
+    }
+
+    // exercise 2
+    public static void printElementsInArray(int [] values) {
+        // error checking
+
+        // selection of the iteration / traversal
+        for (int number : values) {
+            System.out.println(number + " ");
+        }
     }
 
     public static void main(String [] args) {
+        int [] data = new int [] {1,2,3,4,5,6};
 
+        assert (findMax(data) == 6);
+        assert (findMin(data) == 1);
+        assert (computeSum(data)== 21);
+        printElementsInArray(data);
     }
 }
