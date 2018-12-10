6b95a41790e3368f2cfd1d4796c2a38d3f048e56
awkonecki.developer <awkonecki.developer@gmail.com>
2018-12-09 17:35:12 -0800

    [FTR] Exercise 7 - Flip row / col in Square Matrix
    Feature Description
    
    Flipping of the elements within a square matrix across the diagnoal.
    
    Related Issues
    - N/A

--- data-structures/arrays/src/java/IterationTraversal.java
+++ data-structures/arrays/src/java/IterationTraversal.java
@@ -61,16 +61,61 @@ public class IterationTraversal {
 
         // selection of the iteration / traversal
         for (int number : values) {
-            System.out.println(number + " ");
+            System.out.print(number + " ");
         }
     }
 
+    public static void flipSquareMatrix(int [][] matrix) {
+        // error checking 
+        // make sure it is a square
+        // if (matrix.length == matrix[0].length) 
+
+        /*
+            1 2 3
+            4 5 6
+            7 8 9
+
+            1 - is at 0,0
+            5 - is at 1,1
+            9 - is at 9,9
+
+            4 <-> 2
+            3 <-> 7
+            6 <-> 8
+        */
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = row; col < matrix.length; col++) {
+                if (row == col) {
+                    // no work
+                }
+                else {
+                    // work
+
+                    // swap work
+                    int temp = matrix[row][col];
+                    matrix[row][col] = matrix[col][row];
+                    matrix[col][row] = temp;
+                }
+            }
+        }
+
+        for (int [] row : matrix) {
+            printElementsInArray(row);
+            System.out.println("");
+        }
+    }
+
+
     public static void main(String [] args) {
         int [] data = new int [] {1,2,3,4,5,6};
+        int [][] matrix = new int [][] {{1,2,3}, {4,5,6}, {7,8,9}};
 
+        flipSquareMatrix(matrix);
         assert (findMax(data) == 6);
         assert (findMin(data) == 1);
         assert (computeSum(data)== 21);
         printElementsInArray(data);
+        
     }
 }
