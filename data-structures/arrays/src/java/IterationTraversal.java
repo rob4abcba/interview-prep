bff560924f342641f63d4063c8a46eb5b58026e2
awkonecki.developer <awkonecki.developer@gmail.com>
2018-12-09 18:02:53 -0800

    [FTR] Array Modification - Selection Sort
    Feature Description
    
    Implementation of selection sort for array modification exercise 2.
    
    Related Issues
    - N/A

--- data-structures/arrays/src/java/IterationTraversal.java
+++ data-structures/arrays/src/java/IterationTraversal.java
@@ -63,6 +63,7 @@ public class IterationTraversal {
         for (int number : values) {
             System.out.print(number + " ");
         }
+        System.out.println("");
     }
 
     public static void flipSquareMatrix(int [][] matrix) {
@@ -106,16 +107,59 @@ public class IterationTraversal {
         }
     }
 
+    // Exercise 2 - Array Data Manipulation
+    public static void sort(int [] data) {
+        // error checking
+
+        // 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+        // 0, 8, 7, 6, 5, 4, 3, 2, 1, 9
+
+        // selection sort
+
+        // this is performed N times where N is the lenght of data array
+        for (int pivot = 0; pivot < data.length; pivot++) {
+            int min = data[pivot];
+            int minIndex = pivot;
+
+            // perform N - 1 times where N is the lenght of the data array.
+            // staring index - total number of accesses
+            // 0 - 9
+            // 1 - 8
+            // 2 - 7
+            // 3 - 6
+            // ..
+            // 9 - 0
+            // 1 + 2 + 3 + 4 + ... + N - 1 + N - 2
+            // N(N - 1)/2
+            // N^2/2 - N/2
+            // O(N) = O(N^2) for run-time
+            // O(N) = O(C) for memory impact
+            for (int index = pivot + 1; index < data.length; index++) {
+                if (data[index] < min) {
+                    min = data[index];
+                    minIndex = index;
+                }
+            }
+
+            // swap operation
+            int temp = data[pivot];
+            data[pivot] = data[minIndex];
+            data[minIndex] = temp;
+        }
+    }
 
     public static void main(String [] args) {
         int [] data = new int [] {1,2,3,4,5,6};
         int [][] matrix = new int [][] {{1,2,3}, {4,5,6}, {7,8,9}};
+        int [] notSorted = new int [] {6,5,4,3,2,1};
 
         flipSquareMatrix(matrix);
         assert (findMax(data) == 6);
         assert (findMin(data) == 1);
         assert (computeSum(data)== 21);
         printElementsInArray(data);
+        sort(notSorted);
+        printElementsInArray(notSorted);
         
     }
 }
